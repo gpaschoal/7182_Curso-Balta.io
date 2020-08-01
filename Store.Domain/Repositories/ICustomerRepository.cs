b127a18ca12e9fc104c3693ed7c89f31d1488752
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-07-31 21:59:45 -0300

    Created Repositories

---
+++ Store.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Store.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Domain.Repositories
+{
+    interface ICustomerRepository
+    {
+        Customer Get(string document);
+    }
+}
