d0a13d433b0520194b929306e602cf0abbcc0b59
Guilherme Paschoal <paschoal.gui@gmail.com>
2020-08-06 19:56:29 -0300

    Utils + Handles

---
+++ Store.Domain/Handlers/Interfaces/IHandle.cs
@@ -0,0 +1,9 @@
+﻿using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Handlers.Interfaces
+{
+    public interface IHandle<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
